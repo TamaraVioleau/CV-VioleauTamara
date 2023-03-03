b3e97ba9740ca7100eeff5c028a226295c6f3db1
TamaraVioleau <tamara.violeau@gmail.com>
2023-03-03 19:27:12 +0100

    second commit

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -1,46 +1,201 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+<!-- Début du site web -->
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<body class="body__cv">
+    <!-- Corps -->
+    <header class="header__cv">
+        <!-- Navbar -->
+        <nav>
+            <div>
+                <div class="roundfirst"></div>
+                <div class="roundsecond"></div>
+                <div class="roundthird"></div>
+            </div>
+        </nav>
 
-  <div class="card">
-    <Counter />
-  </div>
+        <!-- Titre -->
+        <section>
+            <h1>Tamara VIOLEAU</h1>
+            <div class="section__h2">
+                <img src="https://media-public.canva.com/2l0vA/MAEwp32l0vA/1/t.png" alt="">
+                <div class="title__h2">
+                    <h2>Développeuse front-end</h2>
+                </div>
+                <hr>
+            </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+            <!-- Photo de profil -->
+            <aside>
+                <header class="header__pp">
+                    <i class="header__icon--reduction"></i>
+                    <i class="header__icon--format"></i><i class="header__icon--close"></i>
+                </header>
+                <div class="photo"></div>
+                <div class="text">Contactez-moi.</div>
+                <i class="i fi-rr-angle-down icon--down"></i>
+            </aside>
+        </section>
+    </header>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+
+    <!-- Main -->
+    <main>
+
+        <!-- Contact -->
+ <section class="section__contact">
+            <adress class="adress__contact">
+                <ul class="ul__adress">
+                    <li class="li__adress"><i class="fi fi-rr-phone-call adress__icon"></i><a
+                            href="tel:+33635942113">06.35.94.21.13</a></li>
+                    <li class="li__adress"><i class="fi fi-rr-envelope-open-text adress__icon"></i><a
+                            href="mailto:tamara.violeau@gmail.com">tamara.violeau@gmail.com</a></li>
+                    <li class="li__adress"><i class="fi fi-rr-home adress__icon"></i>29450 SIZUN</li>
+                </ul>
+            </adress>
+            <aside class="aside__social">
+                <ul class="ul__social">
+                    <li class="li__social"><i class="fi fi-brands-linkedin social__icon"></i><a href="#">Linkedin</a></li>
+                    <li class="li__social"><i class="fi fi-brands-github social__icon"></i><a href="#">Github</a></li>
+                    <li class="li__social"></li>
+                </ul>
+            </aside>
+        </section> 
+
+        <!-- SECTION PRESENTATION -->
+
+        <article class="article__wanted">
+            <p>
+                <lord-icon class="wanted__icon" src="https://cdn.lordicon.com/msoeawqm.json" trigger="hover"
+                    style="width:3rem;height:3rem">
+                </lord-icon>Anciennement éducatrice de jeunes enfants, je recherche un <strong>stage de développeur
+                    web</strong>
+                <i>non rémunéré</i>
+                du <strong>25/04 au
+                    24/10/2023</strong> (six mois). Mon expérience dans le domaine social m'a sensibilisée à
+                l'importance de l'accessibilité pour tous, y compris pour les personnes en situation de
+                handicap. Aujourd'hui, <strong>développeuse front-end sensibilisée à l'accessibilité</strong>, je
+                souhaite mettre mes
+                compétences et mes qualités humaines au service de votre entreprise et de vos projets.
+            </p>
+
+        </article>
+
+        <!-- SECTION CV -->
+        <section class="section__cv">
+
+            <wrapper class="wrapper__cvleft">
+                <!-- ARTICLES EXPERIENCES -->
+                <article class="article__experiences">
+                    <div class="cv__search">
+                        <div class="cv__glass">
+                            <lord-icon src="https://cdn.lordicon.com/hgbzryoa.json" trigger="hover"
+                                colors="primary:#000000,secondary:#e4e4e4,tertiary:#b26836" style="height:3rem">
+                            </lord-icon>
+                        </div>
+                        <div class="cv__text">
+                            <h3>Expériences</h3>
+                        </div>
+                    </div>
+
+                    <h4>
+                        oct. 2022 - avr. 2023
+                    </h4>
+
+                    <div class="cv__experience">
+                        <h5>Développeur web</h5>
+                        <p class="p__experience"><i>798 heures intensives</i>
+                            <span class="souligner">Spécialisation : </span>intégration (bundler Vite, Sass, Svelte.js,
+                            CMS Directus) et accessibilité</p>
+                    </div>
+
+                    <h4>
+                        2015 - 2018
+                    </h4>
+
+                    <div class="cv__experience">
+                        <h5>Diplôme d'Etat EJE</h5>
+                        <p class="p__experience">(Éducateur de Jeunes Enfants)</p>
+                    </div>
+
+                    <h4>
+                        2011 -2013
+                    </h4>
+
+                    <div class="cv__experience">
+                        <h5>Bac ST2S</h5>
+                        <p class="p__experience">(Sciences et Technologies de la Santé et du Social)</p>
+                    </div>
+                </article>
+                <!-- ARTICLES SOFT SKILLS -->
+                <article class="article__softskills">
+                    <div class="cv__search">
+                        <div class="cv__glass">
+                            <lord-icon src="https://cdn.lordicon.com/hgbzryoa.json" trigger="hover"
+                                colors="primary:#000000,secondary:#e4e4e4,tertiary:#b26836" style="height:3rem">
+                            </lord-icon>
+                        </div>
+                        <div class="cv__text">
+                            <h3>Expériences</h3>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, minus maiores fugit provident
+                        nam magnam distinctio quidem sed tempora corrupti nihil, voluptatem aliquid! Blanditiis saepe ex
+                        ut, rerum itaque debitis.
+                        Expedita dolorem alias error amet accusamus ipsum, dolor soluta molestiae sunt aliquid
+                        repudiandae eaque cumque? Nesciunt omnis doloribus, quidem veniam iure fugiat consequuntur,
+                        recusandae neque non cupiditate, tempora quisquam ad.</p>
+                </article>
+            </wrapper>
+
+            <wrapper class="wrapper__cvright">
+                <!-- ARTICLE COMPETENCES -->
+                <article class="article__competences">
+                    <div class="cv__search">
+                        <div class="cv__glass">
+                            <lord-icon src="https://cdn.lordicon.com/hgbzryoa.json" trigger="hover"
+                                colors="primary:#000000,secondary:#e4e4e4,tertiary:#b26836" style="height:3rem">
+                            </lord-icon>
+                        </div>
+                        <div class="cv__text">
+                            <h3>Expériences</h3>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, minus maiores fugit provident
+                        nam magnam distinctio quidem sed tempora corrupti nihil, voluptatem aliquid! Blanditiis saepe ex
+                        ut, rerum itaque debitis.
+                        Expedita dolorem alias error amet accusamus ipsum, dolor soluta molestiae sunt aliquid
+                        repudiandae eaque cumque? Nesciunt omnis doloribus, quidem veniam iure fugiat consequuntur,
+                        recusandae neque non cupiditate, tempora quisquam ad.</p>
+                </article>
+                <article class="article__loisirs">
+                    <div class="cv__search">
+                        <div class="cv__glass">
+                            <lord-icon src="https://cdn.lordicon.com/hgbzryoa.json" trigger="hover"
+                                colors="primary:#000000,secondary:#e4e4e4,tertiary:#b26836" style="height:3rem">
+                            </lord-icon>
+                        </div>
+                        <div class="cv__text">
+                            <h3>Expériences</h3>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, minus maiores fugit provident
+                        nam magnam distinctio quidem sed tempora corrupti nihil, voluptatem aliquid! Blanditiis saepe ex
+                        ut, rerum itaque debitis.
+                        Expedita dolorem alias error amet accusamus ipsum, dolor soluta molestiae sunt aliquid
+                        repudiandae eaque cumque? Nesciunt omnis doloribus, quidem veniam iure fugiat consequuntur,
+                        recusandae neque non cupiditate, tempora quisquam ad.</p>
+                </article>
+            </wrapper>
+
+        </section>
+
+    </main>
+
+
+    <!-- Footer -->
+    <footer>
+
+    </footer>
+
+
+</body>
