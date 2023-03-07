a94c8164f5d446621084227e6e57b648b312c782
TamaraVioleau <tamara.violeau@gmail.com>
2023-03-07 13:35:18 +0100

    svg corrigé

--- src/App.svelte
+++ src/App.svelte
@@ -29,8 +29,7 @@
         <div class="photo" />
         <div class="text">Contactez-moi.</div>
         <i class="icon--down"
-          ><img src="./src/assets/images/angle-small-down.svg" alt="" /></i
-        >
+          ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><g id="_01_align_center" data-name="01 align center"><path d="M12,15.5a1.993,1.993,0,0,1-1.414-.585L5.293,9.621,6.707,8.207,12,13.5l5.293-5.293,1.414,1.414-5.293,5.293A1.993,1.993,0,0,1,12,15.5Z"/></g></svg>
       </aside>
     </section>
   </header>
