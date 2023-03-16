6cec73c8592030ce4ae6383f0b374a616ded0245
TamaraVioleau <tamara.violeau@gmail.com>
2023-03-16 17:50:16 +0100

    modification du contenu

--- src/App.svelte
+++ src/App.svelte
@@ -29,7 +29,14 @@
         <div class="photo" />
         <div class="text">Contactez-moi.</div>
         <i class="icon--down"
-          ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><g id="_01_align_center" data-name="01 align center"><path d="M12,15.5a1.993,1.993,0,0,1-1.414-.585L5.293,9.621,6.707,8.207,12,13.5l5.293-5.293,1.414,1.414-5.293,5.293A1.993,1.993,0,0,1,12,15.5Z"/></g></svg>
+          ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+            ><g id="_01_align_center" data-name="01 align center"
+              ><path
+                d="M12,15.5a1.993,1.993,0,0,1-1.414-.585L5.293,9.621,6.707,8.207,12,13.5l5.293-5.293,1.414,1.414-5.293,5.293A1.993,1.993,0,0,1,12,15.5Z"
+              /></g
+            ></svg
+          >
+        </i>
       </aside>
     </section>
   </header>
@@ -37,7 +44,7 @@
   <!-- Main -->
   <main>
     <!-- Contact -->
-    <Contact/>
+    <Contact />
 
     <!-- SECTION PRESENTATION -->
 
@@ -49,15 +56,14 @@
             src="https://cdn.lordicon.com/msoeawqm.json"
             trigger="hover"
             style="width:3rem;height:3rem"
-          />Je recherche un <strong>stage de développeur web</strong>
-          <i>non rémunéré</i>
-          du <strong>25/04 au 24/10/2023</strong> (six mois). Mon expérience en
-          tant que travailleuse sociale, m'a appris l'importance de
-          l'accessibilité pour tous, notamment pour les personnes en situation
-          de handicap. Aujourd'hui,
-          <strong>développeuse front-end sensibilisée à l'accessibilité</strong
-          >, je souhaite mettre mes compétences et mes qualités humaines au
-          service de votre entreprise et de vos projets.
+          />Anciennement éducatrice de jeunes enfants, je me suis reconvertie
+          dans le domaine qui suscite un grand intérêt chez moi depuis toute
+          jeune : le développement web. Aujourd'hui,
+          <strong>développeuse front-end</strong>
+          sensibilisée à l'accessibilité, je souhaite mettre mes compétences au service
+          de votre entreprise et de vos projets. Comment ? Lors d’un
+          <strong>stage</strong>
+          à vos côtés non rémunéré du <strong>25/04 au 24/10/2023</strong>.
         </p>
       </article>
     </div>
@@ -65,8 +71,8 @@
     <!-- SECTION CV -->
     <section class="section__cv">
       <wrapper class="wrapper__cvleft">
-        <!-- ARTICLES EXPERIENCES -->
-        <article class="article__experiences">
+        <!-- ARTICLES FORMATIONS -->
+        <article class="article__formations">
           <div class="cv__search">
             <div class="cv__glass">
               <lord-icon
@@ -77,8 +83,15 @@
               />
             </div>
             <div class="cv__text">
-              <h3>Expériences</h3>
+              <h3>Formations</h3>
             </div>
+          </div>
+
+          <h4>Passage le 07 juin 2023</h4>
+
+          <div class="cv__experience">
+            <h5>Titre Professionnel développeur web et web mobile</h5>
+            <p class="p__experience">(Rennes)</p>
           </div>
 
           <h4>oct. 2022 - avr. 2023</h4>
@@ -88,8 +101,10 @@
             <p class="p__experience">
               École O’clock (téléprésentiel)
               <i>798 heures intensives</i>
+              <i>3 mois de socle commun</i> <i>1 mois de spécialisation</i>
               <span class="souligner">Spécialisation : </span>Intégration i++
-              (bundler Vite, Sass, Svelte.js, CMS Directus) et accessibilité.
+              (bundler Vite, Svelte.js, CMS Directus, maitrise du SASS) et
+              accessibilité.
             </p>
           </div>
 
@@ -112,35 +127,7 @@
             </p>
           </div>
         </article>
-        <!-- ARTICLES SOFT SKILLS -->
-        <article class="article__softskills">
-          <div class="cv__search">
-            <div class="cv__glass">
-              <lord-icon
-                src="https://cdn.lordicon.com/hgbzryoa.json"
-                trigger="hover"
-                colors="primary:#000000,secondary:#e4e4e4,tertiary:#b26836"
-                style="height:3rem width: 3rem;"
-              />
-            </div>
-            <div class="cv__text">
-              <h3>Soft skills</h3>
-            </div>
-          </div>
-          <ul>
-            <li class="patience">Patience</li>
-            <li class="curiosite">Curiosité</li>
-          </ul>
-          <ul><li class="equipe">Travail d'équipe</li></ul>
-          <ul>
-            <li class="adaptabilite">Adaptabilité</li>
-            <li class="observation">Observation</li>
-          </ul>
-          <ul><li class="autonomie">Autonomie</li></ul>
-        </article>
-      </wrapper>
 
-      <wrapper class="wrapper__cvright">
         <!-- ARTICLE COMPETENCES -->
         <article class="article__competences">
           <div class="cv__search">
@@ -168,6 +155,74 @@
           <h5>Gestion de projet numérique</h5>
           <p>Github, Figma, Photoshop, VS Code</p>
         </article>
+      </wrapper>
+
+      <wrapper class="wrapper__cvright">
+        <!-- ARTICLES FORMATIONS -->
+        <article class="article__experiences">
+          <div class="cv__search">
+            <div class="cv__glass">
+              <lord-icon
+                src="https://cdn.lordicon.com/hgbzryoa.json"
+                trigger="hover"
+                colors="primary:#000000,secondary:#e4e4e4,tertiary:#b26836"
+                style="height:3rem width: 3rem;"
+              />
+            </div>
+            <div class="cv__text">
+              <h3>Expériences</h3>
+            </div>
+          </div>
+
+          <h4>avril 2023</h4>
+
+          <div class="cv__experience">
+            <h5>Développeuse Web - O'clock</h5>
+            <p class="p__experience">
+              Projet d'apothéose (fin d'études)
+              <i>4 semaines intensives</i>
+            </p>
+            <div class="list">
+              <ol>
+                <li>Travail d'équipe.</li>
+                <li>Organisation.</li>
+              </ol>
+            </div>
+          </div>
+
+          <h4>2019 - 2021</h4>
+
+          <div class="cv__experience">
+            <h5>Éducatrice de Jeunes Enfants</h5>
+            <p class="p__experience">
+              Crèche de 30 enfants. En continuité de direction.
+            </p>
+            <div class="list">
+              <ol>
+                <li>Ecoute active.</li>
+                <li>Travail d'équipe.</li>
+                <li>Remise en question.</li>
+              </ol>
+              <ol>
+                <li>Diplomatie.</li>
+                <li>Créativité.</li>
+                <li>Adaptabilité.</li>
+              </ol>
+            </div>
+          </div>
+
+          <h4>2011 - 2019</h4>
+
+          <div class="cv__experience">
+            <h5>Animatrice BAFA en colonie de vacances</h5>
+            <p class="p__experience">
+              Animatrice auprès d'enfants de 6 à 17 ans.
+            </p>
+            <p>Animatrice référente 6/8 ans durant deux ans.</p>
+          </div>
+        </article>
+
+        <!-- ARTICLE LOISIRS -->
         <article class="article__loisirs">
           <div class="cv__search">
             <div class="cv__glass">
@@ -196,6 +251,33 @@
               administration d'un forum (codage et management).
             </p>
           </div>
+        </article>
+
+        <!-- ARTICLES SOFT SKILLS -->
+        <article class="article__softskills">
+          <div class="cv__search">
+            <div class="cv__glass">
+              <lord-icon
+                src="https://cdn.lordicon.com/hgbzryoa.json"
+                trigger="hover"
+                colors="primary:#000000,secondary:#e4e4e4,tertiary:#b26836"
+                style="height:3rem width: 3rem;"
+              />
+            </div>
+            <div class="cv__text">
+              <h3>Soft skills</h3>
+            </div>
+          </div>
+          <ul>
+            <li class="patience">Patience</li>
+            <li class="curiosite">Curiosité</li>
+          </ul>
+          <ul><li class="equipe">Travail d'équipe</li></ul>
+          <ul>
+            <li class="adaptabilite">Adaptabilité</li>
+            <li class="observation">Observation</li>
+          </ul>
+          <ul><li class="autonomie">Autonomie</li></ul>
         </article>
       </wrapper>
     </section>
--- src/lib/Boutonscolors.svelte
+++ src/lib/Boutonscolors.svelte
@@ -1,45 +1,59 @@
 <script>
-  function setHeaderColor(color) {
+  function setChangeColor(headerColor, wantedColor, glassColor, ppTopColor, ppBottomColor) {
     const header = document.querySelector(".header__cv");
-    header.style.backgroundColor = color;
+    header.style.backgroundColor = headerColor;
+
+    const wanted = document.querySelector(".article__wanted");
+    wanted.style.borderLeft = wantedColor;
+
+    const glasses = document.querySelectorAll(".cv__glass");
+    for (const glass of glasses) {glass.style.backgroundColor = glassColor;}
+
+    const ppTop = document.querySelector(".header__pp");
+    ppTop.style.backgroundColor = ppTopColor;
+
+    const ppBottom = document.querySelector(".text");
+    ppBottom.style.backgroundColor = ppBottomColor;
   }
 </script>
 
 <aside>
   <div>
-    <div on:click={() => setHeaderColor("#f0be8d")}>
+    <div on:click={() => setChangeColor("#f0be8d", "2rem solid #c5b7dd", "#c4e0e3", "#ffde59", "#ffde59")}>
       <div class="roundfirst" />
     </div>
-    <div on:click={() => setHeaderColor("#ffde59")}>
+    <div on:click={() => setChangeColor("#ffde59", "2rem solid #f0be8d", "#c4e0e3", "#c5b7dd", "#c5b7dd")}>
       <div class="roundsecond" />
     </div>
-    <div on:click={() => setHeaderColor("#c4e0e3")}>
+    <div on:click={() => setChangeColor("#c4e0e3", "2rem solid #f0be8d", "#c5b7dd", "#ffde59", "#ffde59")}>
       <div class="roundthird" />
     </div>
   </div>
 </aside>
 
-<style lang=scss>
-@import "../utils/extends";
-@import "../utils/mixins";
-@import "../utils/variables";
+<style lang="scss">
+  @import "../utils/extends";
+  @import "../utils/mixins";
+  @import "../utils/variables";
 
-   aside {
-      height: 5.5rem;
-      @media screen and (min-width: 1024px) {height:6.5rem;}
+  aside {
+    height: 5.5rem;
+    @media screen and (min-width: 1024px) {
+      height: 6.5rem;
+    }
 
-      div {
-        display: flex;
-        justify-content: right;
-        .roundfirst {
-          @include round($bcg: $color-peach);
-        }
-        .roundsecond {
-          @include round($bcg: $color-yellow);
-        }
-        .roundthird {
-          @include round($bcg: $color-blue);
-        }
+    div {
+      display: flex;
+      justify-content: right;
+      .roundfirst {
+        @include round($bcg: $color-peach);
+      }
+      .roundsecond {
+        @include round($bcg: $color-yellow);
+      }
+      .roundthird {
+        @include round($bcg: $color-blue);
       }
     }
+  }
 </style>
